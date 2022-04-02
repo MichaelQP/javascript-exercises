60037919f9bf4100323241bb6bf945aa22fdf9e9
Michael <99843466+MichaelQP@users.noreply.github.com>
2022-04-01 19:21:42 -0500

    Add files via upload
    Oops , I accidentally added the files in the main repos

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (min < 0 || max < 0) return "ERROR";
+    if (isNaN(min) || isNaN(max)) return "ERROR";
 
+let sum = 0;
+for (let i = min; i < max +1; i++){
+    sum += i;
+}
+return sum;
 };
 
 // Do not edit below this line
